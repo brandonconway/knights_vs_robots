a5664431901158f51ef4cec8c001694d6f1cb586
Brandon Conway <brandoncowboy@gmail.com>
2016-09-10 22:10:21 -0700

    Better scaling for mobile and fullscreen

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import {Game} from './Game.js';
 import {Boot} from './states/Boot.js';
 
 (function () {
-    var game = new Game(800, 600, Phaser.AUTO, "game-container");
+    var game = new Game(800, 450, Phaser.AUTO, "game-container");
     game.state.add('Boot', Boot);
     game.state.start('Boot');
 })();
